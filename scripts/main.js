e002f2ffc72755e7de427f7c714b5c86f9e10650
kilavkiwork <kilavki@outlook.com>
2025-01-17 20:29:23 +0200

    fix: design form

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,12 @@
+// const showPassButton = passwordInput.parentElement.querySelector('.show-hide');
 const fields = document.querySelectorAll('.field');
 const passwordInput = document.querySelector('input[type="password"]');
 const passwordConfirm = document.querySelector(
   'input[type="confirm-password"]'
 );
-const showPassButton = passwordInput.parentElement.querySelector('.show-hide');
-
-// const showPassButton = document.querySelector(
-//   'input[type="password"] + .show-hide'
-// );
-// const showPassConfirmButton = document.querySelector(
-//   'input[type="confirm-password"] + .show-hide'
-// );
+const showPassButton = document.querySelector(
+  'input[type="password"] + .show-hide'
+);
+const showPassConfirmButton = document.querySelector(
+  'input[type="confirm-password"] + .show-hide'
+);
