64611a0adb9bb7844ad3bfb24385c49854f8264e
kilavkiwork <kilavki@outlook.com>
2025-01-18 22:03:17 +0200

    feat: toggle visibility passwords and eyes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,33 @@
 // const showPassButton = passwordInput.parentElement.querySelector('.show-hide');
 const fields = document.querySelectorAll('.field');
-const passwordInput = document.querySelector('input[type="password"]');
-const passwordConfirm = document.querySelector(
-  'input[type="confirm-password"]'
-);
-const showPassButton = document.querySelector(
-  'input[type="password"] + .show-hide'
-);
-const showPassConfirmButton = document.querySelector(
-  'input[type="confirm-password"] + .show-hide'
-);
+const passInputs = document.querySelectorAll('input[type="password"]');
+const eyeButtons = document.querySelectorAll('.eye');
+
+const password = document.querySelector('input[name="password"]');
+const confirmPassword = document.querySelector('input[name="confirm-password"]');
+
+
+//
+// show-hide password and eye
+function toggleVisibility(event) {
+  event.preventDefault();
+  const field = event.target.closest('.field');
+  const input = field.querySelector('input');
+
+  if(input) {
+    input.type = input.type === 'password'
+      ? 'text'
+      : 'password'
+  }
+  
+  field.classList.toggle('hide')  
+}
+
+eyeButtons.forEach((button) => {
+  button.addEventListener('click', toggleVisibility);
+});
+
+//
+// valid email
+//
+
